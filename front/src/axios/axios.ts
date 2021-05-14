925d9e2edb28ca39fcf2cf9609b3cc178bcff1b1
Ernest Rudnicki <rudnicki.ernest@gmail.com>
2021-05-14 19:52:12 +0200

    notification added on errors

--- front/src/axios/axios.ts
+++ front/src/axios/axios.ts
@@ -1,5 +1,6 @@
 import Utils from "../utils/utls";
 import axios from "axios"
+import { notification } from "antd";
 
 const axiosInstance =  axios.create({
   baseURL: process.env.REACT_APP_PROXY_API || 'http://localhost:3001/',
@@ -12,6 +13,10 @@ axiosInstance.interceptors.request.use((config) => {
   }
   return config;
 });
+
+axiosInstance.interceptors.response.use(undefined, (error) => {
+    notification.error({message: error.message, duration: 1});
+})
 
 
 export default axiosInstance
